e05024d86566d0e8115bb195a543c908ee1ddc95
HessahSaedan <hessahsaedan@gmail.com>
2022-02-21 12:41:12 +0300

    MIDTERM1

---
+++ app/src/main/java/com/example/midterm1_se324/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.midterm1_se324;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button playsong;
+    MediaPlayer Song ;
+    int playing;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button playsong  = (Button) findViewById(R.id.button);
+        playing = 0;
+
+    }
+    playsong.OnClickListener songgg = new Button.OnClickListener() {
+
+        @Override
+        public void onClick(View v){
+
+            switch(playing) {
+                case 0:
+                    playsong.start();
+                    playing = 1;
+                    break;
+                case 1:
+                    playsong.pause();
+                    playing = 0;
+                    break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/midterm1_se324/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.example.midterm1_se324;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.BreakIterator;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    mTextField
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // to display the icon inside the app for the bar on top
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_hs);
+        //getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        TextView time = (TextView) findViewById(R.id.time);
+        TimerTask Task = new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        }
+        Timer time = new Timer();
+        time.schedule(Task,9000);
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+                //here you can have your logic to set text to edittext
+            }
+
+            public void onFinish() {
+                mTextField.setText("done!");
+            }
+
+        }.start();
+    }
+}
